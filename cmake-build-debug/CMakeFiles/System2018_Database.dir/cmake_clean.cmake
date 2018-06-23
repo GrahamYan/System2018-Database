6152cec1435015ae4f6a32227cc3dd218472e64d
GrahamYan <yange98@sjtu.edu.cn>
2018-06-23 18:14:04 +0800

    first version clone from ZhouJiuqin

---
+++ cmake-build-debug/CMakeFiles/System2018_Database.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/System2018_Database.dir/src/main.cpp.obj"
+  "CMakeFiles/System2018_Database.dir/src/BpTree/bpt.cc.obj"
+  "System2018_Database.pdb"
+  "System2018_Database.exe"
+  "System2018_Database.exe.manifest"
+  "libSystem2018_Database.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/System2018_Database.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
